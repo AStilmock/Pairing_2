15bb7b0c19432993100e1b5abf63cfdc60c1aa1e
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-23 14:07:28 -0700

    Add second if statement

--- pairing_2/friends.rb
+++ pairing_2/friends.rb
@@ -8,4 +8,13 @@ friends.each do |friend|
     end
 end
 
+another_best_friend = "Johnny"
 
+friends.each do |friend|
+    if friend == best_friend
+        puts "My best friend is #{best_friend}"
+    elsif friend != another_best_friend
+        puts "My #{another_best_friend}"
+    else puts "My other friends are not #{best_friend}"
+    end
+end
