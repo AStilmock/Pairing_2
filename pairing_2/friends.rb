44df8f4b4f721bef1a36503984968ca353bf06dd
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-23 13:57:50 -0700

    Initial commit

---
+++ pairing_2/friends.rb
@@ -0,0 +1,11 @@
+friends = ["Mackenzie", "Tommy", "Timmy"]
+best_friend = "Mackenzie"
+
+friends.each do |friend|
+    if friend == best_friend
+        puts "My best friend is #{best_friend}"
+    else puts "My other friends are not #{best_friend}"
+    end
+end
+
+
